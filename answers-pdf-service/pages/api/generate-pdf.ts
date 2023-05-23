ce6d51f14fe391de453fa684f26c61f610653c67
Igor Correa <igor2.horta@gmail.com>
2023-05-23 11:52:12 -0300

    minor improvements

--- answers-pdf-service/pages/api/generate-pdf.ts
+++ answers-pdf-service/pages/api/generate-pdf.ts
@@ -1,42 +1,28 @@
-import type { GenerateHTMLOptions } from '../../src/types';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getFlowContentJSONFromAPI } from '../../src/flow-content-api';
 import { generateHTML } from '../../src/generate-html';
 import { generatePDF } from '../../src/generate-pdf';
 import { time } from '../../src/utils';
-import * as fs from 'fs';
 import { getPDFParametersFromRequest } from '../../src/request-processing';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { variantRevisionUuid, answers } =
     getPDFParametersFromRequest(req);
 
-  const options: GenerateHTMLOptions = {
-    includeAnswerKeys: false,
-    includeUnanswered: true,
-    includeSelectChoices: true,
-    includeInformationals: true,
-    includeGroups: true,
-    includeSteps: true,
-  };
-
   const flowContent = await getFlowContentJSONFromAPI(variantRevisionUuid);
   const pageHTML = await generateHTML({
     answers,
-    flowContent,
-    options,
+    flowContent
   });
   const pdfBuffer = await time(generatePDF, [pageHTML]);
 
-  await fs.writeFileSync(`./test.pdf`, pdfBuffer, {
-    encoding: 'utf-8'
-  })
-  console.log('done')
-  //const pdfUrl = await uploadBufferToS3(pdfBuffer, `${responderUuid}.pdf`);
+  res.setHeader(
+    'Content-Disposition',
+    `attachment; filename="file.pdf"`
+  );
+  res.setHeader('Content-Type', 'application/pdf');
 
-  return res.json({
-    url: '',
-  });
+  return res.send(pdfBuffer)
 };
 
 export default handler;
