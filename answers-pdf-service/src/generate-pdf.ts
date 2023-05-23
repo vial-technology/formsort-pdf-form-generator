de316b6700877c46e715f217f31c10d72bcf3748
Igor Correa <igor2.horta@gmail.com>
2023-05-23 13:54:54 -0300

    fix puppter issue

--- answers-pdf-service/src/generate-pdf.ts
+++ answers-pdf-service/src/generate-pdf.ts
@@ -1,9 +1,19 @@
-import puppeteer from 'puppeteer';
+import edgeChromium from 'chrome-aws-lambda'
+
+// Importing Puppeteer core as default otherwise
+// it won't function correctly with "launch()"
+import puppeteer from 'puppeteer-core'
+
+const LOCAL_CHROME_EXECUTABLE = ''
 
 export const generatePDF = async (pageContent: string): Promise<Buffer> => {
+  const executablePath = await edgeChromium.executablePath || LOCAL_CHROME_EXECUTABLE
+  
   const browser = await puppeteer.launch({
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
+    executablePath,
+    args: edgeChromium.args,
+    headless: false,
+  })
   const page = await browser.newPage();
 
   await page.setContent(pageContent);
