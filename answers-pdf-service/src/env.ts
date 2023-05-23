0a908a26eb21031149f0d5246b8d9f85b195003a
Igor Correa <igor2.horta@gmail.com>
2023-05-23 12:51:53 -0300

    removing uncessary code

--- answers-pdf-service/src/env.ts
+++ answers-pdf-service/src/env.ts
@@ -1,8 +1,6 @@
 import { z } from 'zod';
 
 const EnvSettings = z.object({
-  // AWS_REGION: z.string().min(1),
-  // AWS_BUCKET_NAME: z.string().min(1),
   FORMSORT_API_KEY: z.string().min(1),
   FORMSORT_API_URL: z.string().url().default('https://api.formsort.com/alpha'),
 });
